efeac92484577b515bf5d153bb4d1ee588dfd04e
freddominugez <frederico.dominguez@gmail.com>
2026-07-05 12:56:39 -0300

    desktop(D7): strings pt-BR e en-US para Qt Linguist
    Cobre CAscApplicationManagerWrapper, CMainWindow, CAboutDialog e CUpdateManager
    substituindo ONLYOFFICE e Euro-Office por Vulcan Office. pt-BR traduz nomes
    dos editores (Documentos, Planilhas, Apresentacoes, PDF). Updater aponta
    usuario para vulcanoffice.com/downloads no lugar do endpoint upstream.
    
    Validado por xmllint. lupdate/lrelease consomem no build do shell Qt.

---
+++ desktop/branding/strings/en-US.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>CAscApplicationManagerWrapper</name>
+    <message>
+        <source>ONLYOFFICE</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Euro-Office</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>ONLYOFFICE Desktop Editors</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Euro-Office Desktop Editors</source>
+        <translation>Vulcan Office</translation>
+    </message>
+</context>
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <source>ONLYOFFICE</source>
+        <translation>Vulcan Office</translation>
+    </message>
+</context>
+<context>
+    <name>CAboutDialog</name>
+    <message>
+        <source>About</source>
+        <translation>About Vulcan Office</translation>
+    </message>
+    <message>
+        <source>View source code</source>
+        <translation>View source code</translation>
+    </message>
+</context>
+<context>
+    <name>CUpdateManager</name>
+    <message>
+        <source>A new version is available</source>
+        <translation>A new version is available at vulcanoffice.com/downloads</translation>
+    </message>
+</context>
+</TS>
---
+++ desktop/branding/strings/pt-BR.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>CAscApplicationManagerWrapper</name>
+    <message>
+        <source>ONLYOFFICE</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Euro-Office</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>ONLYOFFICE Desktop Editors</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Euro-Office Desktop Editors</source>
+        <translation>Vulcan Office</translation>
+    </message>
+</context>
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <source>ONLYOFFICE</source>
+        <translation>Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Document Editor</source>
+        <translation>Editor de Documentos</translation>
+    </message>
+    <message>
+        <source>Spreadsheet Editor</source>
+        <translation>Editor de Planilhas</translation>
+    </message>
+    <message>
+        <source>Presentation Editor</source>
+        <translation>Editor de Apresentacoes</translation>
+    </message>
+    <message>
+        <source>PDF Editor</source>
+        <translation>Editor de PDF</translation>
+    </message>
+</context>
+<context>
+    <name>CAboutDialog</name>
+    <message>
+        <source>About</source>
+        <translation>Sobre o Vulcan Office</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versao</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licenca</translation>
+    </message>
+    <message>
+        <source>View source code</source>
+        <translation>Ver codigo fonte</translation>
+    </message>
+</context>
+<context>
+    <name>CUpdateManager</name>
+    <message>
+        <source>Check for updates</source>
+        <translation>Verificar atualizacoes</translation>
+    </message>
+    <message>
+        <source>A new version is available</source>
+        <translation>Uma nova versao esta disponivel em vulcanoffice.com/downloads</translation>
+    </message>
+</context>
+</TS>
